96b80a08fb233436600b5aaa8139cd6e06aeac2d
Kendrick Ha <hieuhm2610@gmail.com>
2025-06-19 17:05:27 +0700

    feat: add new node types for Lecture and Condition branches
    - Introduced 'custom-lecture', 'custom-condition', and 'condition-branch' node types in index.js to expand node creation options.
    - Updated the nodeTypes export to include new components, enhancing the flexibility and functionality of the flow editor.

--- src/components/nodes/index.js
+++ src/components/nodes/index.js
@@ -14,10 +14,16 @@ import { markRaw } from 'vue';
 import StartNode from './start-node.vue';
 import EndNode from './end-node.vue';
 import SetupNode from './setup/setup-node.vue';
+import LectureNode from './lecture/lecture-node.vue'
+import ConditionNode from './condition/condition-node.vue'
+import ConditionBranchNode from './condition/condition-branch-node.vue'
 
 // TODO: Make sure this will be used consistently across the project, no magic strings.
 export const nodeTypes = {
   'custom-start': markRaw(StartNode),
   'custom-end': markRaw(EndNode),
   'custom-setup': markRaw(SetupNode),
+  'custom-lecture': markRaw(LectureNode),
+  'custom-condition': markRaw(ConditionNode),
+  'condition-branch': markRaw(ConditionBranchNode),
 }; 
