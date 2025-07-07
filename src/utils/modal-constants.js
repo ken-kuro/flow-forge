6736c7358cf3948ca1d3ca2c5f8b79d02899cada
Kendrick Ha <hieuhm2610@gmail.com>
2025-07-07 11:38:48 +0700

    feat: add modal constants for drawing tools and text display types

---
+++ src/utils/modal-constants.js
@@ -0,0 +1,17 @@
+/**
+ * Constants for modal components
+ */
+
+export const DRAWING_TOOLS = {
+  RECTANGLE: 'rectangle',
+  ELLIPSE: 'ellipse',
+  TEXT: 'text'
+};
+
+export const TEXT_DISPLAY_TYPES = {
+  RECTANGLE: 'rectangle',
+  BUBBLE_LEFT: 'bubble-left',
+  BUBBLE_RIGHT: 'bubble-right'
+};
+
+export const MIN_DRAWING_SIZE = 20; // Minimum pixels for a valid drawing
